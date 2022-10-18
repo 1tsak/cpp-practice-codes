c533dffd5dacb7a0791f4e3bd6c6dd7e3ac29239
Aakash Jha <42407874+1tsak@users.noreply.github.com>
2022-10-18 09:46:36 +0530

    Templates

--- templates.cpp
+++ templates.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
-T swapNumbers(T a, T b)
+// template <typename T>
+// T swapNumbers(T a, T b)
+// {
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+// }
+
+// Dynamic array using templates
+template <int T>
+class Array
 {
-    a = a + b;
-    b = a - b;
-    a = a - b;
-    cout<<a<<"\t"<<b;
-}
+private:
+    int arr[T];
+
+public:
+    int getSize()
+    {
+        return T;
+    }
+};
 int main()
 {
-    swapNumbers<int>(2,3);
-    swapNumbers<float>(2.0,3.0);
+    // swapNumbers<int>(2, 3);
+    // swapNumbers<float>(2.0, 3.0);
+    Array<5> arr;
+    cout << arr.getSize();
     return 0;
 }
